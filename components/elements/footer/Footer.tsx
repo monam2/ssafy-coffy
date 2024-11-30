ab4093846e66496b378531ec47bf48d5943fe1b5
monam2 <kangcw0107@gmail.com>
2024-12-01 00:51:42 +0900

    FEAT : 푸터 컴포넌트 의존성 배열 수정

--- components/elements/footer/Footer.tsx
+++ components/elements/footer/Footer.tsx
@@ -3,12 +3,29 @@ import React, { useEffect, useState, useCallback } from "react";
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import { SiMattermost } from "react-icons/si";
 import { getTotalOrderAmount, getTotalOrderCount } from "@/api/firebase";
-import Stats from "./Stats";
 
 const Footer = () => {
   const [footerText, setFooterText] = useState(false);
   const [totalCnt, setTotalCnt] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
+
+  const fetchTotalOrderCnt = useCallback(async () => {
+    try {
+      const totalOrderCnt = await getTotalOrderCount();
+      setTotalCnt((prev) => (prev !== totalOrderCnt ? totalOrderCnt : prev)); // 중복 업데이트 방지
+    } catch (error) {
+      console.error("Failed to fetch total order count:", error);
+    }
+  }, []);
+
+  const fetchTotalOrderPrice = useCallback(async () => {
+    try {
+      const totalOrderPrice = await getTotalOrderAmount();
+      setTotalAmount((prev) => (prev !== totalOrderPrice ? totalOrderPrice : prev)); // 중복 업데이트 방지
+    } catch (error) {
+      console.error("Failed to fetch total order price:", error);
+    }
+  }, []);
 
   const intervalChangeText = useCallback(() => {
     const intervalFunc = setInterval(() => {
@@ -16,16 +33,6 @@ const Footer = () => {
     }, 3000);
 
     return intervalFunc;
-  }, []);
-
-  const fetchTotalOrderCnt = useCallback(async () => {
-    const totalOrderCnt = await getTotalOrderCount();
-    setTotalCnt(totalOrderCnt);
-  }, []);
-
-  const fetchTotalOrderPrice = useCallback(async () => {
-    const totalOrderPrice = await getTotalOrderAmount();
-    setTotalAmount(totalOrderPrice);
   }, []);
 
   const moveTo = (path: string) => {
@@ -45,17 +52,22 @@ const Footer = () => {
   useEffect(() => {
     fetchTotalOrderCnt();
     fetchTotalOrderPrice();
-    const intervalFunc = intervalChangeText();
 
-    return () => {
-      clearInterval(intervalFunc);
-    };
+    const intervalFunc = intervalChangeText();
+    return () => clearInterval(intervalFunc);
   }, [fetchTotalOrderCnt, fetchTotalOrderPrice, intervalChangeText]);
 
   return (
     <div className="w-full h-16 flex justify-between bg-slate-200 dark:bg-slate-600">
-      <Stats footerText={footerText} totalCnt={totalCnt} totalAmount={totalAmount} />
-      <div className=" h-16 flex justify-end pr-10 items-center gap-6">
+      <div className="flex h-full text-lg pl-10 items-center opacity-50 font-[Pretendard] font-bold">
+        {(totalCnt > 0 || totalAmount > 0) &&
+          (footerText ? (
+            <span className="font-bold">누적 주문 수 {totalCnt} 건</span>
+          ) : (
+            <span className="font-bold">총 {totalAmount.toLocaleString()} 원</span>
+          ))}
+      </div>
+      <div className="h-16 flex justify-end pr-10 items-center gap-6">
         <FaGithub onClick={() => moveTo("github")} className="w-7 h-7 opacity-50 cursor-pointer" />
         <FaInstagram
           onClick={() => moveTo("instagram")}
