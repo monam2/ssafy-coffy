9b0e3b99877e91c2d1b830edd86949b583ca9a37
monam2 <kangcw0107@gmail.com>
2025-09-16 23:51:12 +0900

    chore: next 버전 업데이트 및 기타 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // assetPrefix: '.',
@@ -13,4 +15,8 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
