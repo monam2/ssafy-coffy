f7c953bc51393ad84b25b01ab19ba8f92a789160
monam2 <kangcw0107@gmail.com>
2025-09-22 22:59:37 +0900

    chore: supabase db 설정 (client, server)

---
+++ src/shared/config/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createBrowserClient } from "@supabase/ssr";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+let browserClient: SupabaseClient | null = null;
+
+export function getBrowserClient() {
+  if (!browserClient) {
+    browserClient = createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    );
+  }
+  return browserClient;
+}
