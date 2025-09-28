bc357f3634f0401108ff79a4b298a31e397434ac
monam2 <kangcw0107@gmail.com>
2025-09-29 01:14:33 +0900

    nit: import 다시 불러오기

---
+++ src/entities/user/model/types.ts
@@ -0,0 +1,12 @@
+import { DateString } from "@/shared/types";
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  isActive?: boolean;
+  discordWebhookUrl?: string;
+  createdAt?: DateString;
+  updatedAt?: DateString;
+};
