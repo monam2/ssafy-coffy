e4f1c850b6495b82349a85692f172778909acb10
monam2 <kangcw0107@gmail.com>
2025-09-28 21:27:01 +0900

    feat: 버튼 aria-label 추가

--- src/feature/navigate/ui/CartButton.tsx
+++ src/feature/navigate/ui/CartButton.tsx
@@ -5,7 +5,12 @@ import { IoCartOutline } from "react-icons/io5";
 
 const CartButton = () => {
   return (
-    <Button asChild variant="outline" className="w-10 h-10 p-2">
+    <Button
+      asChild
+      variant="outline"
+      className="w-10 h-10 p-2"
+      aria-label="장바구니 페이지 열기"
+    >
       <Link href="/cart">
         <IoCartOutline className="w-full h-full" />
       </Link>
--- src/feature/navigate/ui/OrderHistoryButton.tsx
+++ src/feature/navigate/ui/OrderHistoryButton.tsx
@@ -5,7 +5,12 @@ import { LuClipboardList } from "react-icons/lu";
 
 const OrderHistoryButton = () => {
   return (
-    <Button asChild variant="outline" className="w-10 h-10 p-2">
+    <Button
+      asChild
+      variant="outline"
+      className="w-10 h-10 p-2"
+      aria-label="주문 내역 페이지 열기"
+    >
       <Link href="/order-history">
         <LuClipboardList className="w-full h-full" />
       </Link>
