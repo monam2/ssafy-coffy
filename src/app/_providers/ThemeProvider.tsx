3f16206bf7c1f8c62820c76cd1265aaa2f23e97a
monam2 <kangcw0107@gmail.com>
2025-09-28 23:44:27 +0900

    fix: 타입, import 에러 수정

--- src/app/_providers/ThemeProvider.tsx
+++ src/app/_providers/ThemeProvider.tsx
@@ -2,8 +2,10 @@
 
 import "@/app/globals.css";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,17 @@
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { ReactNode } from "react";
-import { Metadata } from "next";
-import ReactQueryProvider from "@/lib/reactQueryProvider";
+import ThemeProvider from "@/app/_providers/ThemeProvider";
+import ReactQueryProvider from "@/app/_providers/QueryProvider";
+import buildMetadata from "@/shared/config/seo";
 
-export const metadata: Metadata = {
+export const metadata = buildMetadata({
   title: {
     default: "싸피코피",
     template: "%s | 싸피코피",
   },
   description: "광주캠퍼스에서 커피가 마시고 십허요.",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
-  },
-  openGraph: {
-    title: "싸피코피 SSAFY-COFFY",
-    description: "광주캠퍼스에서 커피가 마시고 십허요.",
-    images: [
-      {
-        url: "https://ssafy-cofy.vercel.app/img/logo/thumbnail.png",
-        width: 800,
-        height: 600,
-        alt: "thumbnail",
-        type: "image/png",
-      },
-    ],
-  },
-};
+  ogImage: "/img/logo/thumbnail.png",
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
