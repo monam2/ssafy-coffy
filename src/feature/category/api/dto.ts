6315ddb7357bfca868e2f20799b7ff1921131d6a
monam2 <kangcw0107@gmail.com>
2025-10-08 02:40:26 +0900

    fix: 사용되지 않는 스키마 요소 제거

--- src/feature/category/api/dto.ts
+++ src/feature/category/api/dto.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 /** 카테고리 리스트 요청 스키마 */
-export const getCategoryListReqDtoSchema = z.object({
-  id: z.number().optional(),
-});
+export const getCategoryListReqDtoSchema = z.object({});
 
 export type GetCategoryListReqDto = z.infer<typeof getCategoryListReqDtoSchema>;
 
